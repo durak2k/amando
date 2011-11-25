b76951abb635293b474b6dfb9ff8e6ce3e779a77
andi r <rambonn@gmx.de>
2011-11-25 12:55:06 +0100

    dasdasdadaad

---
+++ src/de/androidbuch/amando/gui/StartseiteAnzeigen.java
@@ -0,0 +1,18 @@
+package de.androidbuch.amando.gui;
+
+
+import de.androidbuch.amando.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartseiteAnzeigen extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		//setContentView(R.layout.);
+		setContentView(R.layout.startseite_anzeigen);
+		
+	}
+
+}
